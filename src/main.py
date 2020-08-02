74c9b2f7fb004efd428215f2c607d2f094f6d14e
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-02 20:15:45 +0530

    Added application entry point

---
+++ src/main.py
@@ -0,0 +1,16 @@
+
+import sys
+from app import Application
+
+app = Application.get_instance(__name__)
+
+
+if __name__ == '__main__':
+    """Entry Point to Application"""
+
+    try:
+        app.run(host=app.config['HOST'], port=app.config['PORT'])
+
+    except Exception as exc:
+        tc, te, tb = sys.exc_info()
+        print('Class: {} | Exception: {} | Line Number: {} | File: {}'.format(tc, exc, tb.tb_lineno, __name__))
