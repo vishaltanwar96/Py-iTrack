f3e5a3a109d833de3904e9d1d2d4672aa1ffe38b
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-03 15:21:05 +0530

    added firstname, lastname and email fields

--- src/models/user.py
+++ src/models/user.py
@@ -7,7 +7,9 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     role = db.relationship('Role', backref='users')
