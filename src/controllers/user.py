22620e10d5c809c375116e561196601ea6c1f26d
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-09 16:32:47 +0530

    Added UserController

---
+++ src/controllers/user.py
@@ -0,0 +1,24 @@
+from flask import views, request, jsonify
+from marshmallow import ValidationError
+
+from utils import db
+from serializers.user import UserRegistrationSerializer
+
+
+class UserController(views.MethodView):
+    """User CRUD Views"""
+
+    def post(self, *args, **kwargs):
+        """Creates a User (User Registration)"""
+
+        if request.is_json():
+            serializer = UserRegistrationSerializer()
+            try:
+                user = serializer.load(**request.get_json())
+                db.session.add(user)
+                db.session.commit()
+                return jsonify({'status': True, 'msg': 'Registration Successful'}), 200
+            except ValidationError as err:
+                db.session.rollback()
+                return jsonify({'status': False, 'msg': err.messages}), 400
+        return jsonify({'status': False, 'msg': 'Invalid JSON'}), 400
