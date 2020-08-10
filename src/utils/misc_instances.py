df450e1a8760dac59b3ec242ef1cc3cb75f889ce
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-10 21:18:22 +0530

    Added JWTManager instance

--- src/utils/misc_instances.py
+++ src/utils/misc_instances.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
+jwt = JWTManager()
