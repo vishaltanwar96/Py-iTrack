ed79cd70275440bd059f36deb1702fb1bef8a518
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-10 22:17:22 +0530

    Added login route

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 
-from controllers.user import UserController
+from controllers.user import UserController, UserLoginController
 
 
 user = Blueprint('name', __name__)
 
 user.add_url_rule('/', view_func=UserController.as_view('user_registration'))
+user.add_url_rule('/login/', view_func=UserLoginController.as_view('user_login'))
