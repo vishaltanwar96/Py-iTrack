223ebdcdeec55981ef981eb150a10dbc7f25eb30
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-13 22:05:04 +0530

    Added UserChangeDetailsSerializer for changing user information

--- src/serializers/user.py
+++ src/serializers/user.py
@@ -65,3 +65,26 @@ class UserSerializer(ma.SQLAlchemyAutoSchema):
         """."""
         model = User
         include_fk = True
+
+
+class UserChangeDetailsSerializer(Schema):
+    """Serializer to Change User Details"""
+
+    first_name = fields.Str(required=False, validate=[validate.Length(min=1, max=50)])
+    last_name = fields.Str(required=False, validate=[validate.Length(min=1, max=50)])
+    email = fields.Email(required=False, validate=[validate.Email()])
+    password = fields.Str(
+        required=False,
+        load_only=True,
+        validate=[
+            validate.Length(min=8, max=40),
+            validate.Regexp(
+                r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+                error="""
+                    Minimum eight characters, at least one uppercase letter, 
+                    one lowercase letter, one number and one special character
+                """
+            )
+        ]
+    )
+    role_id = fields.Int(required=False, validate=[validate.Range(min=0, max=1, max_inclusive=True, min_inclusive=True)])
