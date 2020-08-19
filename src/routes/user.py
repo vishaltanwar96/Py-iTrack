1fb1a9eaf06ab8829116e07e0358eaf914c0a65e
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-19 22:16:14 +0530

    Added PUT method for changing user details

--- src/routes/user.py
+++ src/routes/user.py
@@ -6,6 +6,6 @@ from controllers.user import UserController, UserLoginController
 user = Blueprint('name', __name__)
 
 user_view = UserController.as_view('user_crud')
-user.add_url_rule('/', view_func=user_view, methods=('GET', 'POST'))
+user.add_url_rule('/', view_func=user_view, methods=('GET', 'POST', 'PUT'))
 user.add_url_rule('/<int:user_id>/', view_func=user_view, methods=('GET', 'POST'))
 user.add_url_rule('/login/', view_func=UserLoginController.as_view('user_login'))
