f5a271e4d64fd7a159a58514064647242ac53733
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-22 16:32:11 +0530

    added user_permission association table

--- src/models/association.py
+++ src/models/association.py
@@ -6,3 +6,10 @@ project_owner_table = db.Table(
     db.Column('owner_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('project_id', db.Integer, db.ForeignKey('project.id')),
 )
+
+user_permission_table = db.Table(
+    'user_permission',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id')),
+)
