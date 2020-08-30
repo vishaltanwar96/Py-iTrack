c849429997cd1d4d2cd2709b81afe7ece0d2feda
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-30 20:15:27 +0530

    added organisation table

---
+++ src/models/organisation.py
@@ -0,0 +1,16 @@
+from sqlalchemy import func
+
+from utils import db
+
+
+class Organisation(db.Model):
+
+    __tablename__ = 'organisation'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), nullable=False, unique=True)
+    passcode = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    registered_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    registered_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref='organisation', uselist=False)
