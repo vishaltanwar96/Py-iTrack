0ef0cfe6012cb8295685dede150449c1a075ea54
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-30 20:47:58 +0530

    removed project_owner table

--- src/models/association.py
+++ src/models/association.py
@@ -1,12 +1,5 @@
 from utils import db
 
-project_owner_table = db.Table(
-    'project_owner',
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('owner_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('project_id', db.Integer, db.ForeignKey('project.id')),
-)
-
 user_permission_table = db.Table(
     'user_permission',
     db.Column('id', db.Integer, primary_key=True),
