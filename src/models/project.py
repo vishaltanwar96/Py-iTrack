77f0bedc7ad50c8070c9a4e617bed9f428bc1e73
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-08-30 19:18:00 +0530

    added table remarks_history and metrics

--- src/models/project.py
+++ src/models/project.py
@@ -12,7 +12,29 @@ class Project(db.Model):
     description = db.Column(db.Text, nullable=True, server_default=null())
     status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
     created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+
+
+class ProjectMetrics(db.Model):
+
+    __tablename__ = 'project_metrics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    project = db.relationship('Project', backref='metric', uselist=False)
     pending_tasks = db.Column(db.Integer, nullable=False, server_default=text('0'))
     completed_tasks = db.Column(db.Integer, nullable=False, server_default=text('0'))
     on_hold_tasks = db.Column(db.Integer, nullable=False, server_default=text('0'))
     total_tasks = db.Column(db.Integer, nullable=False, server_default=text('0'))
+
+
+class ProjectRemarksHistory(db.Model):
+
+    __tablename__ = 'project_history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    project = db.relationship('Project', backref='remarks')
+    remarks = db.Column(db.Text, nullable=True, server_default=null())
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='project_remarks')
