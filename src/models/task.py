b6a00060bced87cfb6549816eafad150a3e3e64d
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-01 21:46:31 +0530

    Added a few columns in Task table and added task remarks history table

--- src/models/task.py
+++ src/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import null, func
+from sqlalchemy import null, func, Computed, text
 
 from utils import db
 
@@ -10,16 +10,35 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True, server_default=null())
-    remarks = db.Column(db.Text, nullable=True, server_default=null())
     created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
-    assigned_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    assigned_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    assigned_by = db.relationship('User', foreign_keys=[assigned_by_id])
-    assigned_to = db.relationship('User', foreign_keys=[assigned_to_id])
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    assigned_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    assigned_to = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by_id = db.relationship('User', foreign_keys=[created_by])
+    assigned_by_id = db.relationship('User', foreign_keys=[assigned_by])
+    assigned_to_id = db.relationship('User', foreign_keys=[assigned_to])
     status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    status = db.relationship('Status', backref='tasks')
     criticality_id = db.Column(db.Integer, db.ForeignKey('criticality.id'))
     criticality = db.relationship('Criticality', backref='tasks')
-    status = db.relationship('Status', backref='tasks')
-    expected_closure_by = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    expected_completion_date = db.Column(
+        db.DateTime,
+        server_default=Computed(sqltext=text('created_at + INTERVAL 3 DAY'), persisted=True),
+        nullable=False
+    )
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     project = db.relationship('Project', backref='tasks')
+    actual_completion_date = db.Column(db.DateTime, server_default=null(), nullable=True)
+
+
+class TaskRemarksHistory(db.Model):
+
+    __tablename__ = 'task_history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
+    task = db.relationship('Task', backref='remarks')
+    remarks = db.Column(db.Text, nullable=True, server_default=null())
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    task_creator = db.relationship('User', backref='tasks_created')
