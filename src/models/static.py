aee052b6cc0936119fd83d25031a282e204efe4d
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-06 21:26:13 +0530

    added intial default values to concerned tables

--- src/models/static.py
+++ src/models/static.py
@@ -6,20 +6,32 @@ class Role(db.Model, IdValueMixin):
 
     __tablename__ = 'role'
 
+    initial_values = ('ADMIN', 'MANAGER', 'RESOURCE')
+
 
 class Status(db.Model, IdValueMixin):
 
     __tablename__ = 'status'
+
+    initial_values = ('ASSIGNED', 'WIP', 'ONHOLD', 'IN-REVIEW', 'SCHEDULED', 'COMPLETED', 'ABANDONED')
 
 
 class Criticality(db.Model, IdValueMixin):
 
     __tablename__ = 'criticality'
 
+    initial_values = ('LOW', 'MEDIUM', 'HIGH', 'SEVERE')
+
 
 class Permission(db.Model, IdValueMixin):
 
     __tablename__ = 'permission'
+
+    intial_values = (
+        'CREATE ORGANISATION',
+        'CREATE PROJECT',
+        'CHANGE PROJECT STATUS',
+    )
 
 # Static Models using Metaclasses
 # models = ('role', 'status', 'criticality', 'permission')
