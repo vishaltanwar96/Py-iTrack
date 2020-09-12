cb9c52e96f24a9e32e5403fbb1a33925d593165f
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-12 22:02:08 +0530

    removed role permission association table

--- src/models/association.py
+++ src/models/association.py
@@ -1,11 +1,5 @@
 from utils import db
 
-role_permission_table = db.Table(
-    'role_permission',
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), nullable=False),
-    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'), nullable=False),
-)
 
 user_organisation_table = db.Table(
     'user_organisation',
