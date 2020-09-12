2923e27d17b2a4ac44566f2b444c95a26c220180
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-13 00:36:35 +0530

    added a DELETE method on / route

--- src/routes/user.py
+++ src/routes/user.py
@@ -7,5 +7,5 @@ user = Blueprint('name', __name__)
 
 user_view = UserController.as_view('user_crud')
 user.add_url_rule('/', view_func=user_view, methods=('GET', 'POST', 'PUT'))
-user.add_url_rule('/<int:user_id>/', view_func=user_view, methods=('GET', 'POST'))
+user.add_url_rule('/<int:user_id>/', view_func=user_view, methods=('GET', 'POST', 'DELETE'))
 user.add_url_rule('/login/', view_func=UserLoginController.as_view('user_login'))
