aea80683459342b5dbfc29b2f5df11eb2727e566
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-12 22:01:46 +0530

    removed permission and role permission association table

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 from .project import Project, ProjectMetrics, ProjectRemarksHistory
 from .user import User
 from .task import Task, TaskRemarksHistory
-from .static import Role, Status, Criticality, Permission
+from .static import Role, Status, Criticality
 from .organisation import Organisation
-from .association import role_permission_table, user_organisation_table
+from .association import user_organisation_table
