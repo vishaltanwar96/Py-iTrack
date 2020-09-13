2d286f57e200330b6b5745adff8d58c47c45f7d7
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-13 22:30:30 +0530

    changed 'name' -> 'user'

--- src/routes/user.py
+++ src/routes/user.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from controllers.user import UserController, UserLoginController
 
 
-user = Blueprint('name', __name__)
+user = Blueprint('user', __name__)
 
 user_view = UserController.as_view('user_crud')
 user.add_url_rule('/', view_func=user_view, methods=('GET', 'POST', 'PUT'))
