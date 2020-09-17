d09303939db37ebc1f13751432e6c5ba3c6d3711
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-17 23:13:26 +0530

    removed PUT method from / route

--- src/routes/organisation.py
+++ src/routes/organisation.py
@@ -5,5 +5,5 @@ from controllers.organisation import OrganisationController
 org = Blueprint('organisation', __name__)
 
 org_view = OrganisationController.as_view('org_crud')
-org.add_url_rule('/', view_func=org_view, methods=('GET', 'POST', 'PUT', 'DELETE'))
+org.add_url_rule('/', view_func=org_view, methods=('GET', 'POST', 'DELETE'))
 org.add_url_rule('/<int:org_id>/', view_func=org_view, methods=('GET', 'PUT', 'DELETE'))
