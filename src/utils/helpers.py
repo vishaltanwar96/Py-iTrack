c12d3d620c7e997bffa2f8d3a1cea9d96f93a2b8
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-17 21:37:03 +0530

    configured a jwt function which returns current user on a protected route.

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
 
-from models import Role
-from .misc_instances import db
+from models import Role, User
+from .misc_instances import db, jwt
 
 
 def get_super_users():
@@ -10,3 +10,12 @@ def get_super_users():
     return tuple(
         map(itemgetter(0), db.session.query(Role.id).filter(Role.value.in_(('ADMIN', 'MANAGER'))).all())
     )
+
+
+@jwt.user_loader_callback_loader
+def load_current_user(identity):
+    """:returns user instance loaded from current identity of the protected route"""
+
+    if not identity:
+        return None
+    return User.query.get(identity)
