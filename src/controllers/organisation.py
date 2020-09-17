7dcb3d381372a54b72252a25180fce769eea17db
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-17 23:13:49 +0530

    Added put method to change organisation information

--- src/controllers/organisation.py
+++ src/controllers/organisation.py
@@ -1,14 +1,15 @@
 from marshmallow import ValidationError
 from flask import views, jsonify, request
 from werkzeug.security import generate_password_hash
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_current_user
+from flask_jwt_extended import jwt_required, get_current_user
 
 from utils import db
 from utils.helpers import get_super_users
 from models import Organisation, User, user_organisation_table
 from serializers.organisation import (
     OrganisationSerializer,
-    OrganisationRegistrationSerializer
+    OrganisationRegistrationSerializer,
+    ChangeOrganisationSerializer
 )
 
 
@@ -47,7 +48,7 @@ class OrganisationController(views.MethodView):
             if current_user.role_id not in get_super_users():
                 return jsonify({'status': False, 'msg': 'Action not permissible', 'data': None}), 403
 
-            if db.session.query(user_organisation_table).\
+            if db.session.query(user_organisation_table). \
                     filter(user_organisation_table.c.user_id == current_user.id).first():
                 return jsonify(
                     {'status': False, 'msg': 'A user can be associated with only one organisation', 'data': None}
@@ -76,3 +77,35 @@ class OrganisationController(views.MethodView):
             except ValidationError as err:
                 return jsonify({'status': False, 'msg': 'Validation failed', 'data': None, 'errors': err.messages}), 400
         return jsonify({'status': False, 'msg': 'Invalid JSON', 'data': None}), 400
+
+    def put(self, org_id, *args, **kwargs):
+        """Change organisation information"""
+
+        if not org_id:
+            return jsonify({'status': False, 'msg': 'Organisation ID not specified', 'data': None}), 400
+
+        org = Organisation.query.filter_by(id=org_id)
+
+        if not org.first():
+            return jsonify({'status': False, 'msg': 'Organisation doesn\'t exist', 'data': None}), 404
+
+        if get_current_user().id not in get_super_users():
+            return jsonify({'status': False, 'msg': 'Action not permissible', 'data': None}), 403
+
+        try:
+            request_data = request.get_json()
+            serializer = ChangeOrganisationSerializer()
+            validated_request_data = serializer.load(request_data)
+            org.update(validated_request_data)
+            db.session.commit()
+            return jsonify(
+                {
+                    'status': True,
+                    'msg': 'Details saved successfully',
+                    'data': {'modified_fields': list(request_data.keys())}
+                }
+            ), 200
+        except ValidationError as err:
+            return jsonify(
+                {'status': False, 'msg': 'Validations Failed', 'errors': err.messages, 'data': None}
+            ), 400
