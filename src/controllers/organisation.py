c24eb895ce12c46408714526ae6bb191df4eedc7
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-18 19:29:24 +0530

    used super_users_only decorator instead of manually determining that

--- src/controllers/organisation.py
+++ src/controllers/organisation.py
@@ -1,10 +1,10 @@
 from marshmallow import ValidationError
 from flask import views, jsonify, request
 from werkzeug.security import generate_password_hash
-from flask_jwt_extended import jwt_required, get_current_user
+from flask_jwt_extended import get_current_user
 
 from utils import db
-from utils.helpers import get_super_users
+from utils.helpers import super_users_only
 from models import Organisation, user_organisation_table
 from serializers.organisation import (
     OrganisationSerializer,
@@ -16,7 +16,7 @@ from serializers.organisation import (
 class OrganisationController(views.MethodView):
     """Organisation CRUD Views"""
 
-    decorators = (jwt_required,)
+    decorators = (super_users_only, )
 
     def get(self, org_id=None, *args, **kwargs):
         """Gets all organisations or organisation details based on org_id"""
@@ -44,9 +44,6 @@ class OrganisationController(views.MethodView):
 
             if not current_user:
                 return jsonify({'status': False, 'msg': 'User doesn\'t exist', 'data': None}), 404
-
-            if current_user.role_id not in get_super_users():
-                return jsonify({'status': False, 'msg': 'Action not permissible', 'data': None}), 403
 
             if db.session.query(user_organisation_table). \
                     filter(user_organisation_table.c.user_id == current_user.id).first():
@@ -88,9 +85,6 @@ class OrganisationController(views.MethodView):
 
         if not org.first():
             return jsonify({'status': False, 'msg': 'Organisation doesn\'t exist', 'data': None}), 404
-
-        if get_current_user().id not in get_super_users():
-            return jsonify({'status': False, 'msg': 'Action not permissible', 'data': None}), 403
 
         try:
             request_data = request.get_json()
