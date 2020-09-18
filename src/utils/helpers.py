8b3c4a99fdd0ad33821b9127cd0f3aa522617fab
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-18 19:27:23 +0530

    added decorator super_users_only

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,8 @@
+from functools import wraps
 from operator import itemgetter
+
+from flask import jsonify
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 
 from models import Role, User
 from .misc_instances import db, jwt
@@ -19,3 +23,14 @@ def load_current_user(identity):
     if not identity:
         return None
     return User.query.get(identity)
+
+
+def super_users_only(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        if get_jwt_identity() in get_super_users():
+            return func(*args, **kwargs)
+        else:
+            return jsonify({'status': False, 'msg': 'Action not permissible', 'data': None}), 403
+    return wrapper
