dd1a44368a703489841629e1cf9a12a6fe4f7805
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-19 22:54:01 +0530

    added project blueprint and route to map

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,7 +1,9 @@
 from .user import user
 from .organisation import org
+from .project import project
 
 url_prefix_blueprint = {
     '/api/user': user,
-    '/api/organisation/': org,
+    '/api/organisation': org,
+    '/api/project': project,
 }
