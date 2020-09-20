5ec8dab7d6b471dae00a69ca93d4a9a9a76bdc59
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-20 19:57:21 +0530

    Added user_project_table

--- src/models/association.py
+++ src/models/association.py
@@ -1,9 +1,15 @@
 from utils import db
 
-
 user_organisation_table = db.Table(
     'user_organisation',
     db.Column('id', db.Integer, primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
     db.Column('organisation_id', db.Integer, db.ForeignKey('organisation.id'), nullable=False),
+)
+
+user_project_table = db.Table(
+    'user_project',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id'), nullable=False),
 )
