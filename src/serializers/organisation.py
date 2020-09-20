90d49f5377f8eb04eb501ba03c871dd69db43636
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-20 21:17:57 +0530

    added include_fk in OrganisationSerializer

--- src/serializers/organisation.py
+++ src/serializers/organisation.py
@@ -14,6 +14,7 @@ class OrganisationSerializer(ma.SQLAlchemyAutoSchema, CamelCaseSchema):
         """."""
 
         model = Organisation
+        include_fk = True
         exclude = ('passcode',)
 
 
