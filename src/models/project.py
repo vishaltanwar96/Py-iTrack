a94312a5e392d50d97378bd2a30b2ed8b15d7b3c
vishaltanwar96 <vishal.tanwar@outlook.com>
2020-09-27 20:30:45 +0530

    removed project_metrics table

--- src/models/project.py
+++ src/models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func, text, null
+from sqlalchemy import func, null
 
 from utils import db
 
@@ -18,18 +18,6 @@ class Project(db.Model):
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     project_creator = db.relationship('User', backref='projects_created_by')
     project_users = db.relationship('User', backref='user_projects', secondary='user_project')
-
-
-class ProjectMetrics(db.Model):
-
-    __tablename__ = 'project_metrics'
-
-    id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
-    project = db.relationship('Project', backref='metric', uselist=False)
-    status_id = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
-    status = db.relationship('Status', backref='metrics')
-    count = db.Column(db.Integer, nullable=False, server_default=text('0'))
 
 
 class ProjectRemarksHistory(db.Model):
